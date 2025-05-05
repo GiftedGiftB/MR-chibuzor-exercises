2643f10e8d8b4c7372566e6440cb1410940a22c2
GiftedGift <askhamegift@gmail.com>
2025-05-04 21:23:20 -0800

    'task'

---
+++ Kata.java
@@ -0,0 +1,22 @@
+public class Kata{
+public static boolean isEven(int number){
+
+	 return (number % 2 == 0);
+  
+}
+	public static boolean isPrimeNumber(int number){
+	for (int count = 2; count <= number; count++){
+	if(number % 2 == 0){
+	return false;
+
+}
+}
+	return true;
+
+}
+
+public static boolean subtract(){
+
+}
+}
+
---
+++ TestRun.java
@@ -0,0 +1,11 @@
+public class TestRun{
+	public static void main(String[] args){
+
+
+	//System.out.print("The even number is: "+ Kata.isEven(11));
+
+	//System.out.print(Kata.isPrimeNumber(5));  
+	
+	System.out.print ();
+}
+}					
