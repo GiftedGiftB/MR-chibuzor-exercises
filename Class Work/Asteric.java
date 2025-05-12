fc7faec3c6f1c877d493342b39f2ae04a875748f
GiftedGift <askhamegift@gmail.com>
2025-05-12 11:39:50 -0800

    task done

---
+++ Class Work/Asteric.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Asteric{
+	public static void main(String[] args){
+	Scanner inputScanner = new Scanner(System.in);
+
+
+	for(int i = 1; i <= 5; i++){
+	 for(int g = 1; g <= 5; g++){
+	System.out.print("* *");
+
+	}
+	System.out.println("* *");
+
+
+}
+
+}
+}
