fcd6e0aa2c4dfa0968de282ccaadc6c7b453e153
GiftedGift <askhamegift@gmail.com>
2025-05-13 09:36:13 -0800

    java assignment

--- Java Assignment/MultiplicationAB.java
+++ Java Assignment/MultiplicationAB.java
@@ -3,9 +3,12 @@ public class MultiplicationAB{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
 
-	System.out.print("Enter a number: ");
-	int userNumber = input.nextInt();
+	System.out.print("press a: ");
+	int userNumber1 = input.nextInt();
 
+	if(userNumber1 != a){
+	System.out.print("alphabet 'a' only");
+	}
 }
 
 }
