e76310830274e7a609ba313740c4c71ee7db6576
GiftedGift <askhamegift@gmail.com>
2025-05-14 14:08:30 +0100

    still in progress

--- Java Assignment/MultiplicationAB.java
+++ Java Assignment/MultiplicationAB.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class MultiplicationAB{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
-
-	System.out.print("press a: ");
+	
+	System.out.print("press number: ");
 	int userNumber1 = input.nextInt();
 
-	if(userNumber1 != a){
-	System.out.print("alphabet 'a' only");
+	if(userNumber1 != 1){
+	System.out.print("invalid number");
 	}
 }
 
