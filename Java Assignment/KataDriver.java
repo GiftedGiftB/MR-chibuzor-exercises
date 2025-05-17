b4cc2645e5181d3596787549be70bf13573d290c
GiftedGift <askhamegift@gmail.com>
2025-05-17 10:28:59 +0100

    TDD

---
+++ Java Assignment/KataDriver.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class KataDriver{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	// number 1
+
+	//System.out.print("The even number is: "+ Kata.isEven(10));
+	
+	// number 2
+
+	//System.out.print(Kata.isPrimeNumber(4));
+
+	// number 3
+
+	/* System.out.print("Enter first number: ");
+	int userNumber1 = input.nextInt();
+
+	System.out.print("Enter secound number: ");
+	int userNumber2 = input.nextInt();
+
+	System.out.print(Kata.subtract(userNumber1, userNumber2)); */
+
+	// number 4
+
+	/* System.out.print("Enter first number: ");
+	int firstNumber = input.nextInt();
+	
+	System.out.print("Enter second number: ");
+	int secondNumber = input.nextInt();
+
+	System.out.print("The quotient number is: " + Kata.divide(firstNumber, secondNumber)); */
+
+	// number 5
+
+	System.out.print("Enter a number: ");
+	int userNumber = input.nextInt();
+
+	System.out.print("The number of factor is: " + Kata.factorOf(userNumber));
+
+	// number 6  
+
+	/* System.out.print("Enter a number: ");
+	int SquaredNumber = input.nextInt();
+	System.out.print(Kata.isSquare(SquaredNumber)); */
+
+	// number 7
+
+	
+
+	// number 8
+
+	/* System.out.print("Enter a number: ");
+	int factorialNumber = input.nextInt();
+
+	System.out.print(Kata.factorialOf(factorialNumber)); */
+
+	//number 9
+
+	/* System.out.print("Enter a number: ");
+	int userNumber = input.nextInt();
+
+	System.out.print(Kata.squareOf(userNumber)); */
+}
+}
