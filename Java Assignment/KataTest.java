a83b0b20c13a5d095d106770a936f8b05bf4e7a3
GiftedGift <askhamegift@gmail.com>
2025-05-19 18:06:04 +0100

    task

---
+++ Java Assignment/KataTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class KataTest{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	// number 1
+
+	System.out.print("The even number is: "+ Kata.isEven(11));
+	
+	// number 2
+
+	System.out.print(Kata.isPrimeNumber(5)); 
+
+	// number 6  
+	System.out.print("Enter a number: ");
+	int number = input.nextInt();
+	System.out.print(Kata.isSquare(number));
+}
+}
+
