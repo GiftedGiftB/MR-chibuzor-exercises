057466bc829baf8589731cc7e62fcaa2a512a843
GiftedGift <askhamegift@gmail.com>
2025-05-19 18:04:03 +0100

    task

---
+++ Java Assignment/ArrayKataTesting.java
@@ -0,0 +1,8 @@
+public class ArrayKataTesting{
+	public static void main(String[] args){
+
+	int[] number = {10,15,2,6,20};
+	System.out.print("The largest element in the array is: "+ 	ArrayKata.maximumIn(number));
+ }
+
+}
