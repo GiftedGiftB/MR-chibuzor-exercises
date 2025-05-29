cddc705bae710c60d0a2898c06c95084766ae95a
GiftedGift <askhamegift@gmail.com>
2025-05-29 01:31:28 +0100

    learning

---
+++ My practice folder for java/AccessingArray.java
@@ -0,0 +1,19 @@
+public class AccessingArray{
+	public static void main(String[] args){
+
+
+	/* int[] numbers = new int[3];
+
+	numbers[0] = 5;
+	numbers[1] = 8;
+	numbers[2] = 10; */
+
+	int[] numbers = {6,8,3,6,4};
+	
+	for(int count = 0; count < numbers.length; count++)
+	System.out.print(numbers[count] + " ");
+
+
+	}
+
+}
