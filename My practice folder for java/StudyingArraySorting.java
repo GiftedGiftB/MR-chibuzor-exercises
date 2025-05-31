a3a89a30d132d09b67c396063a03d2b30f4e4b35
GiftedGift <askhamegift@gmail.com>
2025-05-31 19:44:56 +0100

    maximum and minimum number

--- My practice folder for java/StudyingArraySorting.java
+++ My practice folder for java/StudyingArraySorting.java
@@ -1,8 +1,30 @@
+import java.util.Arrays;
 public class StudyingArraySorting{
 	public static void main(String[] args){
 
 	int[] numbers = {5,2,3,-1,0,4,1};
 	System.out.print(Arrays.sort(numbers));
+
+
+	/* int[] numbers = {5,4,3,2,1,0,-1};
+	Arrays.sort((numbers));
+
+
+	char[] characters = {'a','z','b','w','c','A','D','Z','C'};
+	Array.sort(characters);
+
+
+	int[] unicodes = {'a','z','b','w','c','A','D','Z','C'};
+	Array.sort(unicodes); 
+	
+	int[] numbers = {5,4,3,2,1,0,-1};
+	Arrays.sort((numbers));
+
+	System.out.print(Array.binarySearch(numbers,4));
+	System.out.print(Array.binarySearch(numbers,3));
+	System.out.print(Array.binarySearch(numbers,-3)); */
+
+
 
 }
 
