a45c11f500e77ee715be10a3c2dbdde12f6a2dce
GiftedGift <askhamegift@gmail.com>
2025-05-31 01:17:02 +0100

    studying about sorting

--- My practice folder for java/RaggedArray.java
+++ My practice folder for java/RaggedArray.java
@@ -1,5 +1,13 @@
 public class RaggedArray{
+	public static void main(String[] args){
+
+	int[] numbers = {1,2,3,2,4,5};
+
+	for(int i = 0; i < numbers.length; i++)
+		for(int j = 0; j < numbers.length; j++)
+			System.out.print(Array.toString(numbers));
 	
+		System.out.println();
 
-
+  }
 }
---
+++ My practice folder for java/StudyingArraySorting.java
@@ -0,0 +1,9 @@
+public class StudyingArraySorting{
+	public static void main(String[] args){
+
+	int[] numbers = {5,2,3,-1,0,4,1};
+	System.out.print(Arrays.sort(numbers));
+
+}
+
+}
