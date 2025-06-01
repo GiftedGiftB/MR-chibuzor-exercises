9b946f3473292f1d63264eded1e868e206d5de8f
GiftedGift <askhamegift@gmail.com>
2025-06-01 20:50:48 +0100

    also i can add numbers together in the array

--- My practice folder for java/HowToCollectInputWithArray.java
+++ My practice folder for java/HowToCollectInputWithArray.java
@@ -11,12 +11,23 @@ public class HowToCollectInputWithArray{
 
 	int[] array = new int[userNumber];
 
-	System.out.println("Enter numbers in array: ");
+	System.out.print("Enter number: ");
 	for(int i = 0; i < userNumber; i++){
 	array[i] = input.nextInt();
 	}
-
-	
 	System.out.println(Arrays.toString(array));
+	
+	int sum = 0;
+
+	for(int i = 0; i < array.length; i++){
+	
+	sum += array[i];
+	}
+	System.out.print("The sum is: " + sum);
+
+
+
+
+
 }
 }
