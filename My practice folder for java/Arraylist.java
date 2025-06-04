8586147d438b81e15270d8386172323a89dcf1c1
GiftedGiftB <asekhamegift@gmail.com>
2025-06-05 00:48:48 +0100

    Array list

---
+++ My practice folder for java/Arraylist.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Arraylist{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	ArrayList<Integer> integer = new ArrayList<>();
+
+	System.out.println("Enter 5 integer");
+	
+	for(int i = 0; i < 5; i++){
+
+		int temp = input.nextInt();
+
+		if(integers.contains(temp))
+
+			integers.add(temp);
+}
+	collection.sort(integers);
+	System.out.println(integers);
+
+}
+
+ }
+}
