8b3bf1ebcdd417ece916b7114a892333c8fe23a0
GiftedGiftB <asekhamegift@gmail.com>
2025-06-19 00:31:02 +0100

    learnt how to print even number between 1 - 10 using loop only

--- My practice folder for java/ForLoopPractice.java
+++ My practice folder for java/ForLoopPractice.java
@@ -2,8 +2,14 @@ public class ForLoopPractice{
 	public static void main(String[] args){
 
 	
-	for(int count = 1; count <= 10; count++)
-		System.out.print(count);
+	/* for(int count = 1; count <= 10; count++)
+		System.out.print(count); */
+
+	
+		
+	for(int count = 2; count <= 10; count += 2)
+		System.out.print(count + " ");
+
 }
 
 }
