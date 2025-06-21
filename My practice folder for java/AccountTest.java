eadb91ec2e02b2b43b0656898cce79fec6e1491f
GiftedGiftB <asekhamegift@gmail.com>
2025-06-21 19:31:14 +0100

    learning how to make use of Object

--- My practice folder for java/AccountTest.java
+++ My practice folder for java/AccountTest.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 public class AccountTest{
 	public static void main(String[] args){
-	Scanner input = new Sccaner(System.in);
+	Scanner input = new Scanner(System.in);
 
 	Account myAccount = new Account();
 
-	System.out.print(myAccount.getName());
+	System.out.printf("initial name is: %s%n%n", myAccount.getName());
 }
-
-}
-
 
 }
