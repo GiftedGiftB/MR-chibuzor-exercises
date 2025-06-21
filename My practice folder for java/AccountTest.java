b403a3e5f632227e8f358ece167de4b1a61d5169
GiftedGiftB <asekhamegift@gmail.com>
2025-06-21 19:46:07 +0100

    learnt how to use Object to collect name

--- My practice folder for java/AccountTest.java
+++ My practice folder for java/AccountTest.java
@@ -6,6 +6,14 @@ public class AccountTest{
 	Account myAccount = new Account();
 
 	System.out.printf("initial name is: %s%n%n", myAccount.getName());
-}
 
+	System.out.println("please enter the name: ");
+	String theName = input.nextLine();
+	myAccount.setName(theName);
+	System.out.println();
+
+	System.out.printf("Name in object myAccout is:%n%s%n", myAccount.getName());
+
+
+ }
 }
