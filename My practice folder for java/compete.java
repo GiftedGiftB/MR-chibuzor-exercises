83f77ccb3b6624c0693c798c2e0fdb4623ddd80f
GiftedGiftB <asekhamegift@gmail.com>
2025-07-02 23:28:06 +0100

    multiple of 4

--- My practice folder for java/compete.java
+++ My practice folder for java/compete.java
@@ -2,11 +2,17 @@ public class compete{
 	public static void main(String[] args){
 
 
-	for(int count = 79; count >= 0; count--){
+	/* for(int count = 79; count >= 0; count--){
 
 	System.out.println(count);
 
-}
+	} */
+
+	for(int count = 4; count <= 10; count += 4){
+
+	System.out.print(count + " ");
+
+	}
 
 
 
