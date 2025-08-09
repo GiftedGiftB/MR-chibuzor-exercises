3a610e8564c029267f03388dbaf4f895aa10d276
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 22:15:32 +0100

    Question 14

---
+++ My practice folder for java/All Practice/Questionaire/SumOfNumbers14.java
@@ -0,0 +1,14 @@
+public class SumOfNumbers14{
+	public static void main(String[] args){
+
+	int sum = 0;
+
+	for(int count = 0; count <= 10; count++){
+		sum += count;
+		System.out.println(sum);
+	
+	}
+
+}
+
+}
