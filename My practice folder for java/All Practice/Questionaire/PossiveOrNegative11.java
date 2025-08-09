2b815ab322daa33a14c96712b9d679af27ac7091
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 16:41:10 +0100

    Question 11

---
+++ My practice folder for java/All Practice/Questionaire/PossiveOrNegative11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PossiveOrNegative11{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Enter a number: ");
+	int userChoice = input.nextInt();
+
+	if(userChoice > 0){
+	System.out.print("Positive!");
+	} else if(userChoice == 0){
+	System.out.print("Zero!");
+	}else {
+	System.out.print("Negative!");
+	}
+
+}
+
+}
