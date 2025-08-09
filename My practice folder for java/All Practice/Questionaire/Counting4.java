2c5d66bfa622f1b1f53a3ec335c88009528acce3
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 02:05:59 +0100

    Question 4

---
+++ My practice folder for java/All Practice/Questionaire/Counting4.java
@@ -0,0 +1,12 @@
+public class Counting4{
+	public static void main(String[] args){
+
+
+	for(int count = 1; count <= 5; count++){
+		for(int counter = 1; counter <= 5; counter++){
+			System.out.println("Number: " + counter);
+		}
+	}
+}
+
+}
