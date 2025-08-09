03419754c1003c5db2c4b0e09795d38710fafbb7
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 02:39:38 +0100

    Question 6

---
+++ My practice folder for java/All Practice/Questionaire/Multiples6.java
@@ -0,0 +1,13 @@
+public class Multiples6{
+	public static void main(String[] args){
+
+	int number = 15;
+
+	for(int count = 3; count <= number; count++){
+		if(count % 3 == 0){
+			System.out.println(count);
+		}
+	}
+
+}
+}
