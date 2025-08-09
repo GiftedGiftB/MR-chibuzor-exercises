8066d2d8fe1e0ec26f29c27d9d32916775c72619
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 03:02:00 +0100

    Question 7

---
+++ My practice folder for java/All Practice/Questionaire/PasswordChecker7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PasswordChecker7{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	String password = "secret123";
+
+	System.out.print("Enter a password: ");
+	String userInput = input.next();
+
+	if(userInput.equals(password)){
+	System.out.print("Access granted! Welcome!");
+	} else{
+	System.out.print("Access denied!");
+	}
+}
+
+}
