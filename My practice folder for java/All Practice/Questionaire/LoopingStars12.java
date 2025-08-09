6d6fbc34a02fb4bca0677f43ab758d2167be903b
GiftedGiftB <asekhamegift@gmail.com>
2025-08-09 17:08:53 +0100

    Question 13

---
+++ My practice folder for java/All Practice/Questionaire/LoopingStars12.java
@@ -0,0 +1,13 @@
+public class LoopingStars12{
+	public static void main(String[] args){
+
+	int counter = 0;
+
+	for(int count = 1; count <= 5; count++){
+		System.out.println("*");
+		counter++;
+	}
+	System.out.println(counter + " stars");
+}
+
+}
--- My practice folder for java/All Practice/Questionaire/PosiveOrNegative11.java
+++ My practice folder for java/All Practice/Questionaire/PosiveOrNegative11.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PossiveOrNegative11{
+public class PosiveOrNegative11{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
 
