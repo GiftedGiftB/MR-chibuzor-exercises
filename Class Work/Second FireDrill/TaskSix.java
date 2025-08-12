03d007bf3a92978eee8e638bc8e009e6395b57a5
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 13:49:40 +0100

    Task Six

---
+++ Class Work/Second FireDrill/TaskSix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class TaskSix{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	
+	
+
+	int sum = 0;
+	double average = 0;
+	int counter = 0;
+
+	for(int count = 1; count <= 10; count++){
+		System.out.print("Enter score: ");
+		int score = input.nextInt();
+	if(score % 2 == 0){
+		counter++;
+		sum += score;
+		
+	}
+		
+	}
+	average = sum / counter;
+	System.out.println("Average is: " + average);
+
+		
+	
+}
+
+}
