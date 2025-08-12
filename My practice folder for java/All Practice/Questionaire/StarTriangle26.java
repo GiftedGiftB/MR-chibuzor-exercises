8458cdd1dc97c992cb8eb2741ce957d58ab72f50
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 21:40:06 +0100

    Star Triangle

---
+++ My practice folder for java/All Practice/Questionaire/StarTriangle26.java
@@ -0,0 +1,14 @@
+public class StarTriangle26{
+	public static void main(String[] args){
+
+	//int count;
+	
+	for (int count = 1; count <= 5; count++){
+		for (int counter = 1; counter <= count; counter++){
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+
+} 
