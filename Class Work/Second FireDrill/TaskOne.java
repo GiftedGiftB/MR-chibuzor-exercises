515b9d988f0bbdb09db85d8037a72ae0aad7e85a
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 13:03:03 +0100

    collect 10 scores and print sum

--- Class Work/Second FireDrill/TaskOne.java
+++ Class Work/Second FireDrill/TaskOne.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class TaskOne{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
+	
+	
 
 	int sum = 0;
 
-	System.out.print("Enter score: ");
-	int score = input.nextInt();
-
 	for(int count = 1; count <= 10; count++){
+		System.out.print("Enter score: ");
+		int score = input.nextInt();
 		sum += score;
 	}
-	System.out.print(sum);
+	System.out.println("Score is: " + sum);
+	
+	
 }
 
 }
