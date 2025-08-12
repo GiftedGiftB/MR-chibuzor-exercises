051493a1372f70ec68963893a25515e0cf67d7ff
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 13:28:33 +0100

    TaskTwo

---
+++ Class Work/Second FireDrill/TaskTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class TaskTwo{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	
+	
+
+	int sum = 0;
+	double average = 0;
+
+	for(int count = 1; count <= 10; count++){
+		System.out.print("Enter score: ");
+		int score = input.nextInt();
+		sum += score;
+	}
+	average = sum / 10;
+		
+	System.out.println("Average is: " + average);
+	
+	
+}
+
+}
