3fba590bc0a3e8c9756e2a70f72b50c147fe9897
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 23:47:22 +0100

    Divisible by 3 and 5

--- My practice folder for java/All Practice/Questionaire/Divisible27.java
+++ My practice folder for java/All Practice/Questionaire/Divisible27.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
 public class Divisible27{
 	public static void main(String[] args){
-	Scanner input = new Scanner(System.in);
+
 
 	for(int count = 1; count <= 20; count++){
 		if(count % 3 == 0 && count % 5 == 0){
