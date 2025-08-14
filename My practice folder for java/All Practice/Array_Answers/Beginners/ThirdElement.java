1d5f37bab9a2aafb3df6f0be1082bf43e3d2e9dd
GiftedGiftB <asekhamegift@gmail.com>
2025-08-14 14:48:09 +0100

    replaced even numbers with 1 and odd with 0

--- My practice folder for java/All Practice/Array_Answers/Beginners/ThirdElement.java
+++ My practice folder for java/All Practice/Array_Answers/Beginners/ThirdElement.java
@@ -1,9 +1,14 @@
 public class ThirdElement{
 	public static void main(String[] args){
 
-	int[] array = {10, 20, 30, 40, 50};
+	int[] array = {10, 20, 30, 40, 50, 3};
 	for(int count = 0; count < array.length; count++){
-		System.out.println(array[count]);
+		if(array[count] % 2== 0){
+			System.out.println("1");
+		}
+		else{
+		System.out.println("0");
+	}
 	}
 	
 	
