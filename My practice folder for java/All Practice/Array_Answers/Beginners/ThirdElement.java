127ec112f2b78780e36aedfe8fddc566d4980049
GiftedGiftB <asekhamegift@gmail.com>
2025-08-14 14:41:33 +0100

    printed all element

--- My practice folder for java/All Practice/Array_Answers/Beginners/ThirdElement.java
+++ My practice folder for java/All Practice/Array_Answers/Beginners/ThirdElement.java
@@ -2,8 +2,10 @@ public class ThirdElement{
 	public static void main(String[] args){
 
 	int[] array = {10, 20, 30, 40, 50};
+	for(int count = 0; count < array.length; count++){
+		System.out.println(array[count]);
+	}
 	
-	System.out.print(array[2]);
 	
 
 
