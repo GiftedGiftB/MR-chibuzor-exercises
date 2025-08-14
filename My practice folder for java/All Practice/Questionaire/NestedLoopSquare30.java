2646fe133758723788411a122a8014718e592fac
GiftedGiftB <asekhamegift@gmail.com>
2025-08-14 23:32:55 +0100

    Nested loop square

---
+++ My practice folder for java/All Practice/Questionaire/NestedLoopSquare30.java
@@ -0,0 +1,13 @@
+public class  NestedLoopSquare30{
+	public static void main(String[] args){
+
+	for(int count = 1; count <= 4; count++){
+		for(int counter = 1; counter <= 4; counter++){
+			System.out.print("*");
+
+	}
+	System.out.println();
+	}
+	
+}
+}
--- My practice folder for java/All Practice/Questionaire/TrafficLight9.java
+++ My practice folder for java/All Practice/Questionaire/TrafficLight9.java
@@ -3,15 +3,17 @@ public class TrafficLight9{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
 
-	System.out.print("Enter a color like green or red or yellow: ");	String userChoice = input.nextLine().toLowerCase();
+	System.out.print("Enter a color like green or red or yellow: ");
+	String userChoice = input.nextLine().toLowerCase();
 
-	if(userChoice.equals("green")){
-	System.out.println("Go!");
-	} else if(userChoice.equals("red")){
-	System.out.println("Stod!");
-	} else {
-	System.out.println("Wait!");
-	}
+		if(userChoice.equals("green")){
+			System.out.println("Go!");
+		}
+		else if(userChoice.equals("red")){
+			System.out.println("Stod!");
+		} else {
+			System.out.println("Wait!");
+		}
 
 }
 }
