9a00e3de012c0c6d8e3c30a25f1086d84f04a3b8
GiftedGiftB <asekhamegift@gmail.com>
2025-08-16 22:58:01 +0100

    Number Pattern

---
+++ My practice folder for java/All Practice/Questionaire/NumberPattern34.java
@@ -0,0 +1,13 @@
+public class NumberPattern34{
+	public static void main(String[] args){
+
+	for(int count= 1; count <= 3; count++){
+		for(int counter= 1; counter <= count; counter++){
+			System.out.print(counter + " ");
+		}
+	System.out.println();
+
+	}
+}
+
+}
