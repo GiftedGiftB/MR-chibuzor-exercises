1c146a95014e7c0bcceddda30bc5b0a453f25fcd
GiftedGiftB <asekhamegift@gmail.com>
2025-08-19 03:07:41 +0100

    Task Two of fire drill 3

--- Class Work/Third Fire Drill/FireDrill3.java
+++ Class Work/Third Fire Drill/FireDrill3.java
@@ -1,7 +1,9 @@
 public class FireDrill3{
 public static void main(String[] args){
 	taskOne();
-	}
+	taskTwo();
+	
+}
 
 public static void taskOne(){
 	int[] score = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
@@ -9,6 +11,16 @@ public static void taskOne(){
 
 		}
 }
+
+public static void taskTwo(){
+	int[] score = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		for (int count = 0; count < score.length; count++){
+			System.out.println(score[count]);
+
+}
+
+
+		}
 
 
 }
