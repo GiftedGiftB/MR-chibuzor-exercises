0ae5fa5ceb90bc3fe05f8879ab7ea3821c22f8ed
GiftedGiftB <asekhamegift@gmail.com>
2025-10-04 12:31:02 +0100

    working on algorithms as a brain break

--- My practice folder for java/algorithmPractice/SumAllDigits.java
+++ My practice folder for java/algorithmPractice/SumAllDigits.java
@@ -1,9 +1,19 @@
 public class SumAllDigits{
 	public static void main(String[] args){
 
+	// print 0 to 10
+
+	//for(int count = 0; count <= 10; count++){
+	//	System.out.print(count);
+	//}
+
+	// print out just the last number 
+	int sum = 0;
 	for(int count = 0; count <= 10; count++){
-		System.out.print(count);
+		sum++;
 	}
+	System.out.print(sum);
+
  
 }
 }
